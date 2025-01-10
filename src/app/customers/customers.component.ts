0e9735f4d1ba1fd427d639fc3462c22a7ba2b0de
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-10 20:43:45 +0100

    fixed_delete_function

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,58 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {CustomerService} from "../services/customer.service";
-import {catchError, map, Observable, throwError} from "rxjs";
-import {Customer} from "../model/customer.model";
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {Router} from "@angular/router";
+import { CustomerService } from '../services/customer.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Customer } from '../model/customer.model';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.css']
+  styleUrls: ['./customers.component.css'],
 })
 export class CustomersComponent implements OnInit {
-  customers! : Observable<Array<Customer>>;
+  customers!: Observable<Array<Customer>>;
   errorMessage!: string;
-  searchFormGroup : FormGroup | undefined;
-  constructor(private customerService : CustomerService, private fb : FormBuilder, private router : Router) { }
+  searchFormGroup: FormGroup | undefined;
+
+  constructor(
+    private customerService: CustomerService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.searchFormGroup=this.fb.group({
-      keyword : this.fb.control("")
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control(''),
     });
-    this.handleSearchCustomers();
+    this.handleSearchCustomers(); // Initialize customers list on component load
   }
+
   handleSearchCustomers() {
-    let kw=this.searchFormGroup?.value.keyword;
-    this.customers=this.customerService.searchCustomers(kw).pipe(
-      catchError(err => {
-        this.errorMessage=err.message;
+    const kw = this.searchFormGroup?.value.keyword;
+    this.customers = this.customerService.searchCustomers(kw).pipe(
+      catchError((err) => {
+        this.errorMessage = err.message;
         return throwError(err);
       })
     );
   }
 
   handleDeleteCustomer(c: Customer) {
-    let conf = confirm("Are you sure?");
-    if(!conf) return;
+    const conf = confirm('Are you sure?');
+    if (!conf) return;
+
     this.customerService.deleteCustomer(c.id).subscribe({
-      next : (resp) => {
-        this.customers=this.customers.pipe(
-          map(data=>{
-            let index=data.indexOf(c);
-            data.slice(index,1)
-            return data;
-          })
-        );
+      next: () => {
+        // Re-fetch the customer list after deletion
+        this.handleSearchCustomers();
       },
-      error : err => {
-        console.log(err);
-      }
-    })
+      error: (err) => {
+        console.log('Error deleting customer:', err);
+      },
+    });
   }
 
   handleCustomerAccounts(customer: Customer) {
-    this.router.navigateByUrl("/customer-accounts/"+customer.id,{state :customer});
+    this.router.navigateByUrl('/customer-accounts/' + customer.id, { state: customer });
   }
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,25 +1,31 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Customer} from "../model/customer.model";
-import {environment} from "../../environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Customer } from '../model/customer.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomerService {
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public getCustomers():Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>(environment.backendHost+"/customers")
+  public getCustomers(): Observable<Array<Customer>> {
+    return this.http.get<Array<Customer>>(environment.backendHost + '/customers');
   }
-  public searchCustomers(keyword : string):Observable<Array<Customer>>{
-    return this.http.get<Array<Customer>>(environment.backendHost+"/customers/search?keyword="+keyword)
+
+  public searchCustomers(keyword: string): Observable<Array<Customer>> {
+    return this.http.get<Array<Customer>>(
+      environment.backendHost + '/customers/search?keyword=' + keyword
+    );
   }
-  public saveCustomer(customer: Customer):Observable<Customer>{
-    return this.http.post<Customer>(environment.backendHost+"/customers",customer);
+
+  public saveCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(environment.backendHost + '/customers', customer);
   }
-  public deleteCustomer(id: number){
-    return this.http.delete(environment.backendHost+"/customers/"+id);
+
+  // Specify return type as Observable<void> since there is no body expected in response
+  public deleteCustomer(id: number): Observable<void> {
+    return this.http.delete<void>(environment.backendHost + '/customers/' + id);
   }
 }
